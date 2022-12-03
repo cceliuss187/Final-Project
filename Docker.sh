8bdd25a86c66b78ddcbff7c5c8825198e8005630
Mike Major <75327032+mmajor124@users.noreply.github.com>
2022-12-02 20:06:46 -0500

    Add files via upload

---
+++ Docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ~/beaux
+docker exec -ti DockerContainer bash
---
+++ phpScript/Docker.php
@@ -0,0 +1,4 @@
+<?php
+shell_exec("C:\Users\admin\Downloads\Final-Project-main\Docker.sh");
+header('Location: http://127.0.0.1:5500/');
+?>
