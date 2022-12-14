f5a828836456d48ceb85d25482c9eb562c4e3b92
AntonioRios <63029609+Antoniorios17@users.noreply.github.com>
2022-12-13 22:25:12 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,9 +1,12 @@
-from app import app, page_not_found
+from app import app
 
 
 
 #Check to see if the response received is a status code of 404
-def pgNOTfound():
-    response = app.test_client().get('page_not_found.html')
-    assert response.status_code == 404
+# def pgNOTfound():
+#     response = app.test_client().get('page_not_found.html')
+#     assert response.status_code == 404
     
+def test_home_page():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
